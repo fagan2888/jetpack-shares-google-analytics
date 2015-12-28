601cbbd296abfc100ed6731c3ed93b2846cb5144
gregoryfoster <gregoryfoster@users.noreply.github.com>
2015-12-28 15:23:20 -0600

    Switching from let to var

--- analytics-jetpack_shares.js
+++ analytics-jetpack_shares.js
@@ -7,7 +7,7 @@
  */
 jQuery('document').ready(function($){
   if( typeof(ga) == 'function' ){
-    let ajs-networks = [
+    var ajs-networks = [
       { network: 'Twitter',  selector: 'a.share-twitter' },
       { network: 'Facebook', selector: 'a.share-facebook' },
       { network: 'Google',   selector: 'a.share-google-plus-1' },
@@ -16,7 +16,7 @@ jQuery('document').ready(function($){
     ];
 
     // On click, send GA a Social Interaction message
-    for( let target of ajs-networks ){
+    for( var target of ajs-networks ){
       $(target.selector).on('click', function() {
         ga('send', {
           hitType: 'social',
